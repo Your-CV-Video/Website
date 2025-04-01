ad33a96ae304b0a1d419cdbdb270ac0b6ca55a04
Roshan <acc.roshanshrestha@gmail.com>
2025-04-01 01:08:54 +0100

    auth page fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  server: {
-    host: true, // Enables external access
-    port: 4321, // Change if needed
-    allowedHosts: ['.ngrok-free.app'], // Allow all ngrok subdomains
+  build: {
+    css: {
+      code: 'extract', // Extract CSS into separate files
+    },
   },
 });
