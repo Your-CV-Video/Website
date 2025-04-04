6ce2eb53c927f06b901384e12fdcaaae8d20e0bb
Philip Smith <phil@contractsquared.com>
2025-04-04 12:31:14 +0100

    added base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   site: 'https://your-cv-video.github.io',
+  base: 'Website',
   build: {
     css: {
       code: 'extract', // Extract CSS into separate files
