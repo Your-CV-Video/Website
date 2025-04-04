39b6ed30fd395089c1b09e732fa910f6c6288c99
Philip Smith <phil@contractsquared.com>
2025-04-04 12:29:09 +0100

    astrol gihub action

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import partytown from '@astrojs/partytown';
 
 export default defineConfig({
+  site: 'https://your-cv-video.github.io',
   build: {
     css: {
       code: 'extract', // Extract CSS into separate files
